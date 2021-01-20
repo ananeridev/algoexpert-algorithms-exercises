91396d33aecbd38be1d476d4bf84e1b2db695ecf
anabneri <$git_config_user_email>
2021-01-20 11:04:31 -0300

    adding the exercises

---
+++ easy/src/main/java/anabneri/exercises/bubbleSort.java
@@ -0,0 +1,30 @@
+package anabneri.exercises;
+
+public class bubbleSort {
+
+    public static int[] bubbleSort(int[] array) {
+// according to the Big O notation this exercise have 0(n) time
+        if (array.length == 0) {
+            return new int[] {};
+        }
+        boolean isSorted = false;
+        int counter = 0;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1 - counter; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(i, i + 1, array);
+                    isSorted = false;
+                }
+            }
+            counter++;
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+}
---
+++ easy/src/main/java/anabneri/exercises/twoNumberSum.java
@@ -0,0 +1,21 @@
+package anabneri.exercises;
+
+import java.util.*;
+
+// resolve using hash table or Double for loops
+
+public class twoNumberSum {
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int firstNum = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                int secondNum = array[j];
+                if (firstNum + secondNum == targetSum) {
+                    return new int[] {firstNum, secondNum};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
