4ae160c2f689a431c8db01ada62471a4fcfc7b72
anabneri <$git_config_user_email>
2021-01-22 10:55:03 -0300

    fix(): selection sort

---
+++ easy/src/main/java/anabneri/exercises/selectionSort.java
@@ -0,0 +1,12 @@
+package anabneri.exercises;
+
+public class selectionSort {
+    public static int[] selectionSort(int[] array) {
+
+        // found the minor index if the element
+        if (array.length == 0) {
+            return new int[]{};
+        }
+
+    }
+}
