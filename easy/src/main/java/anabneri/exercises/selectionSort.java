c065522b5122737f6ae7619fb6c1dfdf93dfc27d
anabneri <$git_config_user_email>
2021-01-22 11:24:59 -0300

    fix(): finishing the selection sort exercise

--- easy/src/main/java/anabneri/exercises/selectionSort.java
+++ easy/src/main/java/anabneri/exercises/selectionSort.java
@@ -5,8 +5,27 @@ public class selectionSort {
 
         // found the minor index if the element
         if (array.length == 0) {
-            return new int[]{};
+            return new int[] {};
         }
 
+        int startIndex = 0;
+        while (startIndex < array.length - 1) {
+            int smallestIndex = startIndex;
+            for (int i  = startIndex + 1; i < array.length; i++) {
+                if (array[smallestIndex] > array[i]) {
+                    smallestIndex = i;
+                }
+            }
+            // helper function
+            swap(startIndex, smallestIndex, array);
+            startIndex++;
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
     }
 }
