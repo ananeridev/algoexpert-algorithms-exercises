e7915677672fd00283e276d5c29e583a7375c94c
anabneri <ananeri32@hotmail.com>
2021-01-25 16:51:00 -0300

    resolving palindrome check

--- easy/src/main/java/anabneri/palindromeCheck.java
+++ easy/src/main/java/anabneri/palindromeCheck.java
@@ -1,4 +1,16 @@
 package anabneri;
 
 public class palindromeCheck {
+    public static boolean isPalindrome(String str) {
+        // O(n) time complexity and O(1) space complexity
+
+        // store everything of the list in a array
+
+        StringBuilder  reversedString = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedString.append(str.charAt(i));
+        }
+
+        return str.equals(reversedString.toString());
+    }
 }
