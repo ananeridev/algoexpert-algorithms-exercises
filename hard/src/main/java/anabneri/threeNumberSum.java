d3c7ea8db8d5a2e738b85e961f6d4f0d93df5739
anabneri <ananeri32@hotmail.com>
2021-01-26 12:28:55 -0300

    resolving three number sum exercise

--- hard/src/main/java/anabneri/threeNumberSum.java
+++ hard/src/main/java/anabneri/threeNumberSum.java
@@ -1,4 +1,30 @@
 package anabneri;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class threeNumberSum {
+    public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+        Arrays.sort(array);
+        List<Integer[]> triplets = new ArrayList<Integer[]>();
+        for(int i = 0; i < array.length -2; i++) {
+            int left = i + 1;
+            int right = array.length - 1;
+            while (left < right) {
+                int currentSum = array[i] + array[left] + array[right];
+                if (currentSum == targetSum) {
+                    Integer[] newTriplet = {array[i], array[left], array[right]};
+                    triplets.add(newTriplet);
+                    left++;
+                    right++;
+                } else if (currentSum < targetSum) {
+                    left++;
+                } else if (currentSum > targetSum) {
+                    right++;
+                }
+            }
+        }
+        return triplets;
+    }
 }
