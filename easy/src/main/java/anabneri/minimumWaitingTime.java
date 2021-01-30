119d501a63823d85fbb094944c3836eccadc8718
anabneri <ananeri32@hotmail.com>
2021-01-30 17:06:47 -0300

    adding junit dependency and test case

--- easy/src/main/java/anabneri/minimumWaitingTime.java
+++ easy/src/main/java/anabneri/minimumWaitingTime.java
@@ -1,4 +1,8 @@
 package anabneri;
 
 public class minimumWaitingTime {
+    public int minimumWaitingTime(int[] queries) {
+
+        return -1;
+    }
 }
--- easy/src/test/java/anabneri/minimumWaitingTimeTest.java
+++ easy/src/test/java/anabneri/minimumWaitingTimeTest.java
@@ -1,7 +1,8 @@
 package anabneri;
 
-import java.util.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
+import sun.plugin.com.Utils;
 
 public class minimumWaitingTimeTest {
 
@@ -9,7 +10,7 @@ public class minimumWaitingTimeTest {
     public void TestCase1() {
         int[] queries = new int[]{3, 2, 1, 2, 6};
         int expected = 17;
-        var actual = new minimumWaitingTime().minimumWaitingTime(queries);
-        Utils.assertTrue(actual == expected);
+        int actual = new minimumWaitingTime().minimumWaitingTime(queries);
+        Assertions.assertTrue(actual == expected);
     }
 }
