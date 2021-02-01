01eb4cb40c918263c245719cdb9a7bf5946bfc7c
anabneri <ananeri32@hotmail.com>
2021-02-01 18:31:16 -0300

    resolving the fibonacci exercise using an aproach

---
+++ easy/src/test/java/anabneri/nthFibonacci.java
@@ -0,0 +1,16 @@
+package anabneri;
+
+public class nthFibonacci {
+    public static int getNthFib(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 1) {
+            return 0;
+        } else {
+            return getNthFib(n - 1) + getNthFib(n - 2);
+        }
+
+    }
+}
+
