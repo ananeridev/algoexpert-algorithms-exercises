f3a71658ef8fab5dbbd235e11c0d34348cc44842
anabneri <ananeri32@hotmail.com>
2021-02-01 23:00:15 -0300

    adding the Calendar Matching exercise

---
+++ very-hard/src/main/java/anabneri/calendarMatching.java
@@ -0,0 +1,26 @@
+package anabneri;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class calendarMatching {
+    public static List<StringMeeting> calendarMatching(
+            List<StringMeeting> calendar1,
+            StringMeeting dailyBounds1,
+            List<StringMeeting> calendar2,
+            StringMeeting dailyBounds2,
+            int meetingDuration) {
+        // Write your code here.
+        return new ArrayList<StringMeeting>();
+    }
+
+    static class StringMeeting {
+        public String start;
+        public String end;
+
+        public StringMeeting(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
