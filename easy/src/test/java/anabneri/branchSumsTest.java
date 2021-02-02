e5524a37c283a7c4b04588b3745e83668e3543ae
anabneri <ananeri32@hotmail.com>
2021-02-01 22:15:37 -0300

    adding test case for the branch sums program

---
+++ easy/src/test/java/anabneri/branchSumsTest.java
@@ -0,0 +1,49 @@
+package anabneri;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
+public class branchSumsTest extends  branchSums.BinaryTree{
+
+        TestBinaryTree(int value) {
+            super(value);
+        }
+
+        TestBinaryTree insert(List<Integer> values) {
+            return insert(values, 0);
+        }
+
+        TestBinaryTree insert(List<Integer> values, int i) {
+            if (i >= values.size()) return null;
+
+            List<TestBinaryTree> queue = new ArrayList<TestBinaryTree>();
+            queue.add(this);
+            while (queue.size() > 0) {
+                TestBinaryTree current = queue.get(0);
+                queue.remove(0);
+                if (current.left == null) {
+                    current.left = new TestBinaryTree(values.get(i));
+                    break;
+                }
+                queue.add((TestBinaryTree) current.left);
+                if (current.right == null) {
+                    current.right = new TestBinaryTree(values.get(i));
+                    break;
+                }
+                queue.add((TestBinaryTree) current.right);
+            }
+            insert(values, i + 1);
+            return this;
+        }
+    }
+
+    @Test
+    public void TestCase1() {
+        TestBinaryTree tree = new TestBinaryTree(1).insert(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10));
+        List<Integer> expected = new ArrayList<Integer>(Arrays.asList(15, 16, 18, 10, 11));
+        Utils.assertTrue(Program.branchSums(tree).equals(expected));
+    }
+}
