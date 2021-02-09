9249aeab5cf709cc7da494a237fe41171e4080d5
anabneri <ananeri32@hotmail.com>
2021-02-09 12:12:29 -0300

    adding new exercise

---
+++ easy/src/main/java/anabneri/tounamentWinner.java
@@ -0,0 +1,13 @@
+package anabneri;
+
+import java.util.ArrayList;
+
+public class tounamentWinner {
+    public String tournamentWinner(
+            ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+        // Write your code here.
+        return "";
+    }
+}
+
+}
